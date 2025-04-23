ae358da3f8bc0bccfeb74b56148054f445c3a45c
Ekku <lehtoekku@outlook.com>
2025-04-23 04:11:58 +0300

    minor: muistiinpanojen hakemisen siistimistä, ei toiminnallisia muutoksia

--- src/FetchNotes.tsx
+++ src/FetchNotes.tsx
@@ -1,26 +1,30 @@
-import useSWR from "swr"
-import axios from "axios"
-import Note from "./types/Note"
-import { useNoteStore } from "./stores/useNoteStore"
-import { useEffect} from "react";
+import useSWR from "swr";
+import axios from "axios";
+import Note from "./types/Note";
+import { useNoteStore } from "./stores/useNoteStore";
+import { useEffect } from "react";
 
-const fetcher = (url: string) => axios.get(url).then(res => res.data)
+const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 export default function FetchNotes() {
-    const setNotes = useNoteStore((state) => state.setNotes)
+  const setNotes = useNoteStore((state) => state.setNotes);
 
-    const {data: notes, error, isLoading} = useSWR<Note[]>(
-        "https://luentomuistiinpano-api.netlify.app/.netlify/functions/notes", 
-        fetcher,
-        { revalidateOnFocus: false, revalidateOnReconnect: false }
-    )
+  const {
+    data: notes,
+    error,
+    isLoading,
+  } = useSWR<Note[]>(
+    "https://luentomuistiinpano-api.netlify.app/.netlify/functions/notes",
+    fetcher,
+    { revalidateOnFocus: false, revalidateOnReconnect: false }
+  );
 
-    useEffect(() => {
-        if (notes) {
-            setNotes(notes)
-        }
-    }, [notes, setNotes]);
+  useEffect(() => {
+    if (notes) {
+      setNotes(notes);
+    }
+  }, [notes, setNotes]);
 
-    if (error) return <div>failed to load</div>
-    if (isLoading) return <div>loading...</div>
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
 }
